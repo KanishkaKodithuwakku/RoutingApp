edc7f556f97354ec61d3775821ab75265df5e8dd
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-16 00:25:41 +0530

    data layer added

--- src/Context/UsersContext.js
+++ src/Context/UsersContext.js
@@ -1,28 +1,12 @@
 import React, { createContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { usersArray } from "../Data/Data";
+
 
 export const UserContext = createContext();
 const UserContextProvider = ({ children }) => {
   // const [loading, setLoading] = useState(true);
   // const [error, setError] = useState(null);
-  const [users, setUsers] = useState([
-    {
-      id: uuidv4(),
-      fullname: "Kalhari Gallage",
-      email: "kalhari@gmail.com",
-      gender: "Female",
-      address: "Passara",
-      zipcode: "0453",
-    },
-    {
-      id: uuidv4(),
-      fullname: "Kanishka Bandara Kodithuwakku",
-      email: "kanishka@gmail.com",
-      gender: "Male",
-      address: "Badulla",
-      zipcode: "0123",
-    },
-  ]);
+  const [users, setUsers] = useState(usersArray);
 
   const contextValue = { users, setUsers };
 
---
+++ src/Data/Data.js
@@ -0,0 +1,20 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const usersArray = [
+  {
+    id: uuidv4(),
+    fullname: "Kalhari Gallage",
+    email: "kalhari@gmail.com",
+    gender: "Female",
+    address: "Passara",
+    zipcode: "0453",
+  },
+  {
+    id: uuidv4(),
+    fullname: "Kanishka Bandara Kodithuwakku",
+    email: "kanishka@gmail.com",
+    gender: "Male",
+    address: "Badulla",
+    zipcode: "0123",
+  },
+];
